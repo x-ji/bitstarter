5143bb89937ef752167d26a8ec0b0d378bc02475
AnonJ <jimmyjx51@gmail.com>
2013-07-08 03:02:51 +0000

    asdf

--- web.js
+++ web.js
@@ -11,14 +11,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
